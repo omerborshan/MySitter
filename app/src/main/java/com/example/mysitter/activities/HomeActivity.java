e999d758aee6b908d2b9d863dae4373f7e322f01
NetanelHp <pour55555@gmail.com>
2021-02-17 16:22:52 +0200

    NavBar + Fragments

---
+++ app/src/main/java/com/example/mysitter/activities/HomeActivity.java
@@ -0,0 +1,62 @@
+package com.example.mysitter.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.mysitter.R;
+import com.example.mysitter.fragments.AboutFragment;
+import com.example.mysitter.fragments.EmployeeFragment;
+import com.example.mysitter.fragments.RegisterFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new EmployeeFragment()).commit();
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()){
+                        case R.id.nav_list:
+                            selectedFragment = new EmployeeFragment();
+                            break;
+                        case R.id.nav_register:
+                            selectedFragment = new RegisterFragment();
+                            break;
+                        case R.id.nav_about:
+                            selectedFragment = new AboutFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
+
+    public void loadRegisterFragment() {
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, new RegisterFragment()).addToBackStack(null).commit();
+    }
+}
